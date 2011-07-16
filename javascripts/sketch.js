6b7818f53fe5a13a52ab016757128fe3351d14c0
Noah H. Smith <nsmith@rendergardenmedia.com>
2011-07-16 09:52:51 -0500

    Introducing the sketch literal framework

---
+++ javascripts/sketch.js
@@ -0,0 +1,25 @@
+$(function() {
+  $.template("frame-description",
+  '<div class="suppress_all frame-description">\
+    <span class="tag_name">{{if tag_name}}${tag_name}{{/if}}</span>\
+    <span class="id">{{if id}}${id}{{/if}}</span>\
+    <span class="class">{{if class}}${class}{{/if}}</span>\
+  </div>');
+  $.template("_id", '<span class="id">{{if id}}${id}{{/if}}</span>');
+  $.template("_class", '<span class="class">{{if class}}${class}{{/if}}</span>');
+  
+  var options = {
+    ignore: '.suppress, .suppress_all, a, div'
+  }
+  $('body *').not(options.ignore).each(function(){
+    var
+      description = {
+        tag_name: this.tagName.toLowerCase(),
+        id: $(this).attr('id'),
+        class: $(this).attr('class')
+      },
+      content = $.tmpl("frame-description",description);
+      
+    $(this).prepend(content);
+  });
+});
