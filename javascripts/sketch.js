b537f24dffe731c6a60ab2fc8c5e23c3523f4d36
Noah H. Smith <nsmith@rendergardenmedia.com>
2011-07-17 02:18:01 -0500

    Quite a bit of javascript restructuring, plus a few new styles to make up for jQuery template stripping spaces

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,18 +1,34 @@
 $(function() {
-  $.template("frame-description", '<div class="suppress_all frame-description">{{if tag_name}}<span class="tag_name">${tag_name}</span>{{/if}}{{if id}} <span class="id">${id}</span>{{/if}}{{if class}} <span class="class">${class}</span>{{/if}}</div>');
+  $.template("frame-description", '<div class="suppress_all frame-description">{{html $item.html}}</div>');
+  $.template("valid",
+    '{{wrap "frame-description"}}\
+      {{if tag_name}}\
+        {{tmpl "tag_name"}}\
+      {{/if}}\
+      {{if id}}\
+        $nbsp;{{tmpl "id"}}\
+      {{/if}}\
+      {{if class}}\
+        $nbsp;{{tmpl "class"}}\
+      {{/if}}\
+    {{/wrap}}');
+  $.template("tag_name", '<span class="tag_name">${tag_name}</span>');
+  $.template("id", '<span class="id">${id}</span>');
+  $.template("class", '<span class="class">${class}</span>');
   
   var options = {
     ignore: '.suppress, .suppress_all, .suppress_all *, a, div'
   }
+
   $('body *').not(options.ignore).each(function(){
     var
       description = {
         tag_name: this.tagName.toLowerCase(),
         id: $(this).attr('id'),
         class: $(this).attr('class')
-      },
-      content = $.tmpl("frame-description",description);
-      
+      };
+    
+    content = $.tmpl("valid",description);
     $(this).prepend(content);
   });
 });
